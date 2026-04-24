fe3d4efbbf1491335fe7a2cce4b804f17e2cf301
agung-permana <agungpermana490@gmail.com>
2026-04-24 08:50:27 +0700

    master qa

---
+++ database/seeders/MasterQaToUserSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+class MasterQaToUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // 1. Ambil semua data dari master_qa
+        $masterQa = DB::table('masterqa')->get();
+
+        foreach ($masterQa as $data) {
+            $email = Str::lower(str_replace(' ', '.', $data->nama_qa)) . '@ni.com';
+
+            // 3. Masukkan ke tabel users
+            DB::table('users')->updateOrInsert(
+                ['code_qa' => $data->code_qa], // Cek apakah code_qa sudah ada (biar tidak duplikat)
+                [
+                    'name'      => $data->nama_qa,
+                    'email'     => $email,
+                    'password'  => Hash::make('ni123456'), 
+                    'role_id'   => 1, // Sesuaikan dengan role (misal: 2 = QAL)
+                    'created_at' => now(),
+                    'updated_at' => now(),
+                ]
+            );
+        }
+    }
+}
